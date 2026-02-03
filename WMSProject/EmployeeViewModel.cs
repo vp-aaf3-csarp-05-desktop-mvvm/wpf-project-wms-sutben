1636e728db1765c3e3176a53f3034efc4a2b7b09
Rbencuska69 <116081300+Bence38@users.noreply.github.com>
2026-02-03 10:40:32 +0100

    adószám és ki nem adott szabadságok kiiratása

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -3,5 +3,7 @@
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string TaxNum { get; } = "12345678-2-10";
+        public string KiNemAdottSzabadnap { get; } = "5";
     }
 }
