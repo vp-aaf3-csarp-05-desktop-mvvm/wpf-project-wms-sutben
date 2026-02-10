40df4f2203938bd12801210b19f1e00adde257b1
Rbencuska69 <116081300+Bence38@users.noreply.github.com>
2026-02-10 10:24:51 +0100

    Viewmodel három property-vel ipari megjegyzéssel

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -1,9 +1,23 @@
 ﻿namespace WMSProject
 {
+    /// <summary>
+    /// Dolgozó adatok megjelenítéshez (szabadnapkezelés) 
+    /// </summary>
     public class EmployeeViewModel
     {
+        /// <summary>
+        /// Dolgozó neve
+        /// </summary>
         public string Name { get; } = "Nagy Anna";
-        public string TaxNum { get; } = "12345678-2-10";
-        public string KiNemAdottSzabadnap { get; } = "5";
+
+        /// <summary>
+        /// Adószáma
+        /// </summary>
+        public string TaxNum { get; } = "12345678 - 2 - 10";
+
+        /// <summary>
+        /// Kivett szabadnapok száma
+        /// </summary>
+        public int DaysOff { get; } = 6;
     }
 }
